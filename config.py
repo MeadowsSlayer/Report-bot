fa962f9f10519fd3ba5d8247f6d130801abcf5ba
Djamal <djama2134@gmail.com>
2017-08-14 21:45:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,26 +1,3 @@
 token='381985593:AAE-e9pz7fiJNdDl7gBtCJxQTJfPWrkIIuI'
 
-
-action2 = "0"
-a = "0"
-r = "0"
-v = "0"
-z = "0"
-action = "0"
-place = "0"
-road = "0"
-u = "0"
-jo = 0
-UserName_2 = "0"
-UserID_1 = "0"
-first = "0"
-second = "0"
-third = "0"
-UserID = 0
 Rights = 0
-b = "0"
-c = "0"
-d = "0"
-e = "0"
-f = "0"
-g = "0"

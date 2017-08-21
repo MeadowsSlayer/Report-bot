4dfc29ec73e25070a11acaa3bdeeda16be2726d6
Djamal <djama2134@gmail.com>
2017-08-21 10:24:05 +0300

    Update time_thread.py

--- time_thread.py
+++ time_thread.py
@@ -4,7 +4,7 @@ from ff import *
 def message_send():
     while True:
         now_time = datetime.datetime.now()
-        if (now_time.hour == 10 and now_time.minute == 24) or (now_time.hour == 9) or (now_time.hour == 13) or (now_time.hour == 18):
+        if (now_time.hour == 10 and now_time.minute == 25) or (now_time.hour == 9) or (now_time.hour == 13) or (now_time.hour == 18):
             for beta in user:
                 buttions(beta)
 

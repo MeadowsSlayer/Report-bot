3efad269fb57e2b18c2c6fe1dfe868fe18022def
MeadowsSlayer <djama2134@gmail.com>
2017-08-26 11:13:41 +0300

    Last Change

--- bot.py
+++ bot.py
@@ -51,17 +51,19 @@ def welcome_message(message):
         
 
 def add_user(message):
-    connect = connect()
-    cursor = connect.cursor()
+    connects = connect()
+    cursor = connects.cursor()
     cursor.execute("SELECT UserID FROM User WHERE UserID=%s" % (int(message.chat.id)))
     result = cursor.fetchone()
     cursor.close()
+    connects.close()
     if result is None:
-        cursor = conn.cursor()
+        connects = connect()
+        cursor = connects.cursor()
         cursor.execute("INSERT INTO User (UserName,UserID,LastMessage) VALUES(%r,%r,%i)" % (str(message.chat.first_name), str(message.chat.id),1))
-        conn.commit()
+        connects.commit()
         cursor.close()
-        connect.close()
+        connects.close()
         
 
 
@@ -93,11 +95,15 @@ def new_user_name(message):
 # Развитие событий "Я на объекте"
 @bot.message_handler(regexp="Я на объекте")
 def handle_message(message):
+    connects = connect()
+    cursor = connects.cursor()
     workup = types.ReplyKeyboardMarkup()
     workup.add('Объект новый', 'Объект старый')
     bot.send_message(message.chat.id, "На каком объекте?", reply_markup=workup)
     cursor.execute("INSERT INTO Table_2(UserID,Action,Message) VALUES(%s,'Я на объекте','Last')" % (int(message.chat.id)))
-    conn.commit()
+    connects.commit()
+    cursor.close()
+    connects.close()
 
 @bot.message_handler(regexp="Объект новый")
 def handle_message_id(message):
@@ -106,12 +112,13 @@ def handle_message_id(message):
 
 @bot.message_handler(regexp="Объект старый")
 def answer_text(message):
-    connect = connect()
-    cursor = connect.cursor()
+    connects = connect()
+    cursor = connects.cursor()
     cursor.execute("SELECT Buttion FROM UserButtion WHERE UserID=%i AND OP='O'" % (message.chat.id))
-    [results] = cursor.fetchone()
+    if cursor.fetchone() is not None:
+        [results] = cursor.fetchone()
     cursor.close()
-    connect.close()
+    connects.close()
     user_markup = types.ReplyKeyboardMarkup()
     for beta in but:
         user_markup.row(str(results))
@@ -119,41 +126,42 @@ def answer_text(message):
     bot.register_next_step_handler(msg, object_obj)
 
 def object_end(message):
-    connect = connect()
-    cursor = connect.cursor()
+    connects = connect()
+    cursor = connects.cursor()
     cursor.execute("SELECT UserName FROM User WHERE UserID = %i" % (int(message.chat.id)))
     UseName_2 = cursor.fetchall()
     cursor.execute("SELECT Rights FROM User WHERE UserID = %i" % (int(message.chat.id)))
     UserID_1 = cursor.fetchall()
     cursor.execute("UPDATE Table_2 SET UserName=%r, Rights=%r, DateTime=%r, Action2=%r ,Message=' ' WHERE UserID=%s AND Message='Last'" %
                    (str(UseName_2), str(UserID_1), time.asctime(), message.text, int(message.chat.id)))
-    conn.commit()
+    connects.commit()
     cursor.close()
-    connect.close()
+    connects.close()
     bot.send_message(message.chat.id, "Я запомню это")
 
 def object_obj(message):
-    connect = connect()
-    cursor = connect.cursor()
+    connects = connect()
+    cursor = connects.cursor()
     msg = bot.send_message(message.chat.id, "Что вы там делаете?")
     bot.register_next_step_handler(msg, object_end)
     cursor.execute("UPDATE Table_2 SET PlaceProject = %r WHERE UserID=%s AND Message='Last'" %
                    (str(message.text), int(message.chat.id)))
-    conn.commit()
-    connect.close()
+    connects.commit()
+    connects.close()
+    cursor.close()
 
 def object_obj_2(message):
-    connect = connect()
-    cursor = connect.cursor()
+    connects = connect()
+    cursor = connects.cursor()
     msg = bot.send_message(message.chat.id, "Что вы там делаете?")
     bot.register_next_step_handler(msg, object_end)
     but.append(message.chat.id)
     cursor.execute("INSERT INTO UserButtion VALUES(%s,'O',%r)" % (int(message.chat.id), message.text))
     cursor.execute("UPDATE Table_2 SET PlaceProject = %r WHERE UserID=%s AND Message='Last'" %
                    (message.text, int(message.chat.id)))
-    conn.commit()
+    connects.commit()
     cursor.close()
-    connect.close()
+    connects.close()
 
 
 
@@ -165,13 +173,13 @@ def mess(message):
     buttion_new = types.KeyboardButton(text="Я занимаюсь чем-то новым")
     buttion_old = types.KeyboardButton(text="Я все еще работаю над одним из старых проектов")
     work.add(buttion_new, buttion_old)
-    connect = connect()
-    cursor = connect.cursor()
+    connects = connect()
+    cursor = connects.cursor()
     bot.send_message(message.chat.id, "Что вы там делаете?", reply_markup=work)
     cursor.execute("INSERT INTO Table_2(UserID,Action,Message) VALUES(%s,'Я в лаюоротории','Last')" % (int(message.chat.id)))
-    conn.commit()
+    connects.commit()
     cursor.close()
-    connect.close()
+    connects.close()
 
 @bot.message_handler(regexp="Я занимаюсь чем-то новым")
 def new(message):
@@ -180,8 +188,8 @@ def new(message):
 
 @bot.message_handler(regexp="Я все еще работаю над одним из старых проектов")
 def handle_message_id(message):
-    connect = connect()
-    cursor = connect.cursor()
+    connects = connect()
+    cursor = connects.cursor()
     cursor.execute("SELECT Buttion FROM UserButtion WHERE UserID=%i AND OP='P'" % (message.chat.id))
     [results] = cursor.fetchone()
     cursor.close()
@@ -190,38 +198,38 @@ def handle_message_id(message):
         maxup.row(str(results))
     msg = bot.send_message(message.chat.id, "Напишите, чем именно вы сейчас заняты", reply_markup=maxup)
     cursor.close()
-    connect.close()
+    connects.close()
     bot.register_next_step_handler(msg, project_pro)
 
 def project_pro(message):
-    msg = bot.send_message(message.chat.id, "Что вы там делаете?")
-    connect = connect()
-    cursor = connect.cursor()
+    msg = bot.send_message(message.chat.id, "Над чем вы работаете?")
+    connects = connect()
+    cursor = connects.cursor()
     bot.register_next_step_handler(msg, object_end)
     cursor.execute("UPDATE Table_2 SET PlaceProject = %r WHERE UserID=%s AND Message='Last'" %
                    (str(message.text), int(message.chat.id)))
-    conn.commit()
+    connects.commit()
     cursor.close()
-    connect.close()
+    connects.close()
 
 
 def project_pro_2(message):
     msg = bot.send_message(message.chat.id, "Что вы там делаете?")
     bot.register_next_step_handler(msg, object_end)
-    connect = connect()
-    cursor = connect.cursor()
+    connects = connect()
+    cursor = connects.cursor()
     but_p.append(message.chat.id)
     cursor.execute("INSERT INTO UserButtion VALUES(%s,'P',%r)" % (int(message.chat.id), str(message.text)))
     cursor.execute("UPDATE Table_2 SET PlaceProject = %r WHERE UserID=%s AND Message='Last'" %
                    (message.text, int(message.chat.id)))
-    conn.commit()
+    connects.commit()
     cursor.close()
-    connect.close()
+    connects.close()
 
 
 def project(message):
-    connect = connect()
-    cursor = connect.cursor()
+    connects = connect()
+    cursor = connects.cursor()
     cursor.execute("SELECT UserName FROM User WHERE UserID = %i" % (int(message.chat.id)))
     UserName_2 = cursor.fetchall()
     cursor.execute("SELECT Rights FROM User WHERE UserID = %i" % (int(message.chat.id)))
@@ -229,9 +237,9 @@ def project(message):
     cursor.execute(
         "UPDATE Table_2 UserName=%r, Rights=%r, DateTime=%r, Action2=%r ,Message=' ' WHERE UserID=%s AND Message='Last'" %
         (str(UserName_2), str(UserID_1), time.asctime(), message.text, int(message.chat.id)))
-    conn.commit()
+    connects.commit()
     cursor.close()
-    connect.close()
+    connects.close()
     bot.send_message(message.chat.id, "Я запомню это")
 
 
@@ -239,18 +247,18 @@ def project(message):
 # Реакция на "В дороге"
 @bot.message_handler(regexp="Я в дороге")
 def road_mess(message):
-    connect = connect()
-    cursor = connect.cursor()
+    connects = connect()
+    cursor = connects.cursor()
     msg = bot.send_message(message.chat.id, "Куда вы едете?")
     cursor.execute("INSERT INTO Table_2(UserID,Message) VALUES(%s,'Last')" % (int(message.chat.id)))
-    conn.commit()
+    connects.commit()
     cursor.close()
-    connect.close()
+    connects.close()
     bot.register_next_step_handler(msg, road_why)
 
 def road(message):
-    connect = connect()
-    cursor = connect.cursor()
+    connects = connect()
+    cursor = connects.cursor()
     cursor.execute("SELECT UserName FROM User WHERE UserID = %i" % (int(message.chat.id)))
     UserName_2 = cursor.fetchall()
     cursor.execute("SELECT Rights FROM User WHERE UserID = %i" % (int(message.chat.id)))
@@ -258,18 +266,18 @@ def road(message):
     bot.send_message(message.chat.id, "Я запомню это")
     cursor.execute("UPDATE Table_2 SET UserName=%r, Rights=%r, DateTime=%r, Action='Я в Дороге', Action2='В дороге', ForRoad=%r, Message='Not Last' WHERE UserID=%s AND Message='Last'" %
                     (str(UserName_2), str(UserID_1), time.asctime(), message.text, int(message.chat.id)))
-    conn.commit()
+    connects.commit()
     cursor.close()
-    connect.close()
+    connects.close()
 
 def road_why(message):
-    connect = connect()
-    cursor = connect.cursor()
+    connects= connect()
+    cursor = connects.cursor()
     msg = bot.send_message(message.chat.id, "Зачем?")
     cursor.execute("UPDATE Table_2 SET PlaceProject=%r WHERE UserID=%s AND Message='Last'" % (message.text, int(message.chat.id)))
-    conn.commit()
+    connects.commit()
     cursor.close()
-    connect.close()
+    connects.close()
     bot.register_next_step_handler(msg, road)
 
 
@@ -277,25 +285,25 @@ def road_why(message):
 # Реакция на "Другое"
 @bot.message_handler(regexp="Другое")
 def other(message):
-    connect = connect()
-    cursor = connect.cursor()
+    connects = connect()
+    cursor = connects.cursor()
     cursor.execute("SELECT UserName FROM User WHERE UserID = %i" % (int(message.chat.id)))
     UserName_2 = cursor.fetchall()
     cursor.execute("SELECT Rights FROM User WHERE UserID = %i" % (int(message.chat.id)))
     UserID_1 = cursor.fetchall()
     cursor.execute("INSERT INTO Table_2(UserName,UserID, Rights, DateTime,Action,Message) VALUES(%r,%s,%r,%r,%r,'Last')" % (str(UserName_2), message.chat.id, str(UserID_1), time.asctime(), message.text))
     cursor.close()
-    connect.close()
+    connects.close()
     msg = bot.send_message(message.chat.id, "Напишите.")
     bot.register_next_step_handler(msg, other_o)
 
 def other_o(message):
-    connect = connect()
-    cursor = connect.cursor()
+    connects = connect()
+    cursor = connects.cursor()
     cursor.execute("UPDATE Table_2 SET Action2=%r, Message=' ' WHERE UserID=%s AND Message='Last'" % (message.text, message.chat.id))
-    conn.commit()
+    connects.commit()
     cursor.close()
-    connect.close()
+    connects.close()
     bot.send_message(message.chat.id,"Я запомню это")
 
 
@@ -303,8 +311,8 @@ def other_o(message):
 # Реакция на "Дома"
 @bot.message_handler(regexp="Я дома")
 def other(message):
-    connect = connect()
-    cursor = connect.cursor()
+    connects = connect()
+    cursor = connects.cursor()
     cursor.execute("SELECT UserName FROM User WHERE UserID = %i" % (int(message.chat.id)))
     UserName_2 = cursor.fetchall()
     cursor.execute("SELECT Rights FROM User WHERE UserID = %i" % (int(message.chat.id)))
@@ -313,9 +321,9 @@ def other(message):
     place = "0"
     cursor.execute("INSERT INTO Table_2 VALUES(%r,%i,%r,%r,%r,%r,%r,%r,%r)" %
                    (str(UserName_2), message.chat.id, str(UserID_1), time.asctime(), action, place, "Я дома", "",""))
-    conn.commit()
+    connects.commit()
     cursor.close()
-    connect.close()
+    connects.close()
     bot.send_message(message.chat.id, "Ок")
 
 
@@ -323,14 +331,14 @@ def other(message):
 # Удаление проектов
 @bot.message_handler(regexp="Я хочу удалить проект")
 def delete(message):
-    connect = connect()
-    cursor = connect.cursor()
+    connects = connect()
+    cursor = connects.cursor()
     cursor.execute("SELECT Buttion FROM UserButtion WHERE UserID=%i AND OP='P'" % (int(message.chat.id)))
     maxup = types.ReplyKeyboardMarkup(row_width=1)
     for message.chat.id in but_p:
         maxup.add(cursor.fetchone())
     cursor.close()
-    connect.close()
+    connects.close()
     msg = bot.send_message(message.chat.id, "Выберите проект на удаление", reply_markup=maxup)
     bot.register_next_step_handler(msg, delete_project)
 
@@ -343,24 +351,24 @@ def delete_project(message):
 # Удаление объектов
 @bot.message_handler(regexp="Я хочу удалить объект")
 def delete(message):
-    connect = connect()
-    cursor = connect.cursor()
+    connects = connect()
+    cursor = connects.cursor()
     cursor.execute("SELECT Buttion FROM UserButtion WHERE UserID=%i AND OP='P'" % (int(message.chat.id)))
     maxup = types.ReplyKeyboardMarkup(row_width=1)
     for message.chat.id in but:
         maxup.add(cursor.fetchone())
     cursor.close()
-    connect.close()
+    connects.close()
     msg = bot.send_message(message.chat.id, "Выберите объект на удаление", reply_markup=maxup)
     bot.register_next_step_handler(msg, delete_object)
 
 
 def delete_object(message):
-    connect = connect()
-    cursor = connect.cursor()
+    connects = connect()
+    cursor = connects.cursor()
     cursor.execute("DELETE FROM UserButtion WHERE UserID=%i AND Buttion=%s" % (int(message.chat.id), message.text))
     cursor.close()
-    connect.close()
+    connects.close()
 
 e1 = threading.Event()
 
--- time_thread.py
+++ time_thread.py
@@ -20,7 +20,7 @@ def message_send():
 		cursor.close()
 		connects.close()
 		now_time = datetime.datetime.now()
-		if (now_time.hour == 9) or (now_time.hour == 13) or (now_time.hour == 18):
+		if (now_time.hour == 9) or (now_time.hour == 12) or (now_time.hour == 18):
 			for row in result:
 				if str(b) in str(count):
 					time.sleep(3600) 
@@ -29,10 +29,9 @@ def message_send():
 				buttion_1 = types.KeyboardButton(text="Я на объекте")
 				buttion_2 = types.KeyboardButton(text="Я в дороге")
 				buttion_3 = types.KeyboardButton(text="Я дома")
-				buttion_4 = types.KeyboardButton(text="Я в лаборотории")
+				buttion_4 = types.KeyboardButton(text="Я в лаборатории")
 				buttion_5 = types.KeyboardButton(text="Другое")
 				make.add(buttion_1, buttion_2, buttion_3, buttion_4, buttion_5)
 				bot.send_message(row[i], "Где вы или чем заняты?", reply_markup=make)
 				++i
 				b+=1
-				print(b)
